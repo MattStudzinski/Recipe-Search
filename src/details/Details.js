55071e948b7871ab82ea67211909f7e435c52f1b
mattstudzinski <matthewstudzinski@yahoo.com>
2022-07-22 09:40:16 -0700

    Details css done

--- src/details/Details.js
+++ src/details/Details.js
@@ -27,11 +27,11 @@ if(!foundRecipe) {
         <div className='more-details'>
             <img className='food-im' src={foundRecipe.recipe.image} alt= {foundRecipe.label}/>
             <p className='calo'>Calories: {foundRecipe.recipe.calories} </p>
-            <p className='type'>{foundRecipe.recipe.cuisineType}</p>
-            <p className='mealType'>{foundRecipe.recipe.mealType}</p>
-            <p className='cautions'>{foundRecipe.recipe.cautions}</p>
-            <p className='diet'>{foundRecipe.recipe.dietLabels}</p>
-            <p className='time'>{foundRecipe.recipe.totalTime}</p>
+            <p className='type'>Cuisine Type: {foundRecipe.recipe.cuisineType}</p>
+            <p className='mealType'>Meal Type: {foundRecipe.recipe.mealType}</p>
+            <p className='cautions'>Be wary of: {foundRecipe.recipe.cautions}</p>
+            <p className='diet'>Special diet needs: {foundRecipe.recipe.dietLabels}</p>
+            <p className='time'>Time to cook: {foundRecipe.recipe.totalTime} minutes</p>
         </div>
     )
 };
